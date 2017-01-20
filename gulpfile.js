b15224c856bf16b47426290d2052ee9876015365
Ricky Noel Diancin Jr <rickyxmarter@gmail.com>
2017-01-20 15:34:14 +0800

    modifiend gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,26 @@ var gulp = require('gulp'),
   webserver = require('gulp-webserver');
 
 gulp.task('js', function() {
-  gulp.src('builds/angularregistration/js/**/*');
+  gulp.src('builds/development/js/**/*');
 });
 
 gulp.task('html', function() {
-  gulp.src('builds/angularregistration/*.html');
+  gulp.src('builds/development/*.html');
 });
 
 gulp.task('css', function() {
-  gulp.src('builds/angularregistration/css/*.css');
+  gulp.src('builds/development/css/*.css');
 });
 
 gulp.task('watch', function() {
-  gulp.watch('builds/angularregistration/js/**/*', ['js']);
-  gulp.watch('builds/angularregistration/css/*.css', ['css']);
-  gulp.watch(['builds/angularregistration/*.html',
-    'builds/angularregistration/views/*.html'], ['html']);
+  gulp.watch('builds/development/js/**/*', ['js']);
+  gulp.watch('builds/development/css/*.css', ['css']);
+  gulp.watch(['builds/development/*.html',
+    'builds/development/views/*.html'], ['html']);
 });
 
 gulp.task('webserver', function() {
-  gulp.src('builds/angularregistration/')
+  gulp.src('builds/development/')
     .pipe(webserver({
       livereload: true,
       open: true
